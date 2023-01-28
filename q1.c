5792f52f4cdc203a3e519187def1bbd91567fc47
itsankittiwari <ankittiwari2282000@gmail.com>
2023-01-28 16:00:20 +0530

    first file pushed

---
+++ q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // maxium of two number
+    int a, b;
+    scanf("%d%d",&a,&b);
+    if(a > b)
+    {
+       printf("sghs %d",a);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
