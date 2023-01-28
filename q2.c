dc1e31322f3bad8c9b7f0c189fc17835e79834db
itsankittiwari <ankittiwari2282000@gmail.com>
2023-01-28 16:21:09 +0530

    second file pushed

---
+++ q2.c
@@ -0,0 +1,19 @@
+//Write a C program to find maximum between three numbers.
+#include <stdio.h>
+
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c){
+    printf("%d",a);
+}
+else if(b>a && b>c){
+    printf("%d",b);
+}
+else{
+    printf("%d",c);
+}
+
+   
+}
