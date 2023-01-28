6109ed6dde7117a8150e68b3f896cc1b32081225
itsankittiwari <ankittiwari2282000@gmail.com>
2023-01-28 20:24:44 +0530

    twenty file pushed

---
+++ q20.c
@@ -0,0 +1,32 @@
+/*Write a C program to input basic salary of an employee and calculate its Gross salary according to following:
+Basic Salary <= 10000 : HRA = 20%, DA = 80%
+Basic Salary <= 20000 : HRA = 25%, DA = 90%
+Basic Salary > 20000 : HRA = 30%, DA = 95%
+*/
+#include <stdio.h>
+int main()
+{
+    int B_S, HRA, DA, G_S;
+    scanf("%d", &B_S);
+    if (B_S <= 10000)
+    {
+        HRA = B_S * 0.20;
+        DA = B_S * 0.80;
+        G_S = B_S + HRA + DA;
+        printf("%d", G_S);
+    }
+    else if (B_S <= 20000)
+    {
+        HRA = B_S * 0.25;
+        DA = B_S * 0.90;
+        G_S = B_S + HRA + DA;
+        printf("%d", G_S);
+    }
+    else
+    {
+        HRA = B_S * 0.30;
+        DA = B_S * 0.95;
+        G_S = B_S + HRA + DA;
+        printf("%d", G_S);
+    }
+}
